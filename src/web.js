72c7c633d63fde04dd9a71151d24671ec0958813
SonicCoding1180 <noahsturner06@gmail.com>
2019-04-17 15:22:35 -0400

    Add files via upload

--- src/web.js
+++ src/web.js
@@ -6,17 +6,3 @@ app.use('/web', express.static(__dirname+'/web'));
 app.get('/web/favicon.ico', (req, res) => {
 	res.send(express.static(__dirname+'/web/favicon.ico'));
 });
-const ServerReq = require('./structures/Server.js'/*'./serverold.js'*/);
-const fs = require('fs');
-const Server = new ServerReq();
-
-Server.on('connection', (ws) => {
-	console.log('Client connected.');
-	ws.on('message', (msg) => {
-		try {
-			Server.handleData(ws, JSON.parse(msg));
-		} catch(err) {
-			console.log(err);
-		}
-	});
-});
