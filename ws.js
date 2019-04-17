533b0fc0a433ddc8d823f0cde10ae3fb9be5c47b
SonicCoding1180 <noahsturner06@gmail.com>
2019-04-16 20:56:46 -0400

    Add files via upload

--- ws.js
+++ ws.js
@@ -1,4 +1,12 @@
-const ServerReq = require('./src/structures/Server.js'/*'./serverold.js'*/);
+const Config = require('./structures/config.json');
+const express = require('express');
+const app = express();
+app.listen(process.env.PORT || Config.port);
+app.use('/web', express.static(__dirname+'/web'));
+app.get('/web/favicon.ico', (req, res) => {
+	res.send(express.static(__dirname+'/web/favicon.ico'));
+});
+const ServerReq = require('./structures/Server.js'/*'./serverold.js'*/);
 const fs = require('fs');
 const Server = new ServerReq();
 
